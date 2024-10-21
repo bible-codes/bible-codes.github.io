eadc63b58550a7a122ddaa6bbebb685cd1e9b05f
roni762583 <roni762583@gmail.com>
2024-10-21 19:11:36 -0400

    pwa

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'bible-code-app-v1';
+const urlsToCache = [
+  './',
+  'img/favicon.png',
+  'data/torahNoSpaces.txt',
+  'js/test.js',
+  // Add other assets that need to be cached
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
